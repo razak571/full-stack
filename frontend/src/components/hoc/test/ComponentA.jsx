4082692927d76151e7313cfcebd791854d51319c
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-22 19:11:47 +0530

    useMemo

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -1,14 +1,31 @@
-import { useFetchData } from "./useDataFetch";
-
+import { useState, useMemo } from "react";
 function ComponentA() {
-  const { data, loading, error } = useFetchData(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  );
+  const expensiveCalculation = (num) => {
+    console.log("calculating hevy task", num);
+    let result = 0;
+    for (let i = 0; i < 1e16; i++) {
+      result += num;
+      return result;
+    }
+  };
 
-  if (loading) return <pre> {JSON.stringify(loading)} </pre>;
-  if (error) return <pre> {JSON.stringify(error.message)} </pre>;
-  console.log(data);
-  return <pre> {JSON.stringify(data, null, 2)} </pre>;
+  const [count, setCount] = useState(0);
+  // console.log(expensiveCalculation(count));
+  const [inc, setInc] = useState(1);
+
+  const memoizedValue = useMemo(() => expensiveCalculation(count), [count]);
+
+  console.log(memoizedValue);
+
+  return (
+    <>
+      <h1>
+        count:{count} || inc: {inc}{" "}
+      </h1>
+      <button onClick={() => setCount(count + 1)}>count + </button> <br />
+      <button onClick={() => setInc(inc + 1)}>inc + </button>
+    </>
+  );
 }
 
 export default ComponentA;
