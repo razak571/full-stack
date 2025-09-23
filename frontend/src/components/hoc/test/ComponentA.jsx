8683f759bd02ded0f8bc6c7f9d30e07ed7b26fb6
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-23 19:26:00 +0530

    useRef

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -1,29 +1,25 @@
-import { useState, useMemo } from "react";
+import { useState, useRef } from "react";
+
 function ComponentA() {
-  const expensiveCalculation = (num) => {
-    console.log("calculating hevy task", num);
-    let result = 0;
-    for (let i = 0; i < 1e16; i++) {
-      result += num;
-      return result;
-    }
+  // useEffect = to run side effects i.e. calling api,
+
+  const [count, setCount] = useState(10);
+
+  const mutableCount = useRef(0);
+
+  const updateMutable = () => {
+    mutableCount.current = mutableCount.current + 1;
+    console.log(mutableCount.current);
   };
-
-  const [count, setCount] = useState(0);
-  // console.log(expensiveCalculation(count));
-  const [inc, setInc] = useState(1);
-
-  const memoizedValue = useMemo(() => expensiveCalculation(count), [count]);
-
-  console.log(memoizedValue);
 
   return (
     <>
       <h1>
-        count:{count} || inc: {inc}{" "}
+        count: {count} || mutable count: {mutableCount.current}{" "}
       </h1>
-      <button onClick={() => setCount(count + 1)}>count + </button> <br />
-      <button onClick={() => setInc(inc + 1)}>inc + </button>
+      <button onClick={() => setCount(count + 1)}>count+</button>
+      <br />
+      <button onClick={updateMutable}>mutable+</button>
     </>
   );
 }
