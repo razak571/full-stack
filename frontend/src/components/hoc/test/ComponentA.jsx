10f0627ef5f4844ea5217bd6ccc0a38c674af8ac
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-25 22:34:09 +0530

    useEffect

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -1,38 +1,17 @@
 import { useEffect, useState } from "react";
 
 function ComponentA() {
-  let url = "https://jsonplaceholder.typicode.com/todos/1";
-
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [count, setCount] = useState(5);
 
   useEffect(() => {
-    const dataFetch = async () => {
-      try {
-        setLoading(true);
-        setError(null);
-        const res = await fetch(url);
-        if (!res.ok) {
-          throw Error("Res.ok not true");
-        }
-        const resData = await res.json();
-        setData(resData);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-        console.log(error);
-      }
-    };
-    dataFetch();
-  }, [url]);
+    setInterval(() => {
+      setCount((prev) => Math.max(prev - 1, 0));
+    }, 1000);
+  }, []);
 
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error hai:: {error}</h1>;
   return (
     <>
-      <h1>{data?.title} </h1>
+      <h1>Count: {count} </h1>
     </>
   );
 }
