84a8514fadf78fc1f12420bf3ac4f799ae49d6d0
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-25 19:37:31 +0530

    useReducer

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -1,16 +1,27 @@
-import ComponentB from "./ComponentB";
-import { useSharedData } from "./UseSharedData";
-function ComponentA() {
-  const shared = useSharedData();
+import { useReducer } from "react";
 
-  const handleClick = () => {
-    console.log(shared.notify("Message from Component A"));
-  };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1, name: state.name };
+    case "decrement":
+      return { count: state.count - 1, name: state.name };
+    default:
+      return state;
+  }
+};
+
+function ComponentA() {
+  const [state, dispatch] = useReducer(reducer, { count: 0, name: "Razak" });
 
   return (
     <>
-      <button onClick={handleClick}>Pass Data To Component B</button>
-      <ComponentB />
+      <h1>
+        Count : {state.count} || name: {state.name}{" "}
+      </h1>
+      <button onClick={() => dispatch({ type: "increment" })}>Count ++</button>{" "}
+      <br />
+      <button onClick={() => dispatch({ type: "decrement" })}>Count --</button>
     </>
   );
 }
