fd5ba66c35b95cfdaaaf4b96740f8f2cfc07e36d
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-26 18:39:52 +0530

    Todo: done task

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -5,10 +5,12 @@ function ComponentA() {
     {
       id: 1,
       task: "react",
+      done: false,
     },
     {
       id: 2,
       task: "node",
+      done: false,
     },
   ]);
 
@@ -17,7 +19,11 @@ function ComponentA() {
   const createTask = () => {
     // const date = new Date();
     // console.log(date);
-    const newTask = { id: idRef.current, task: `task ${idRef.current}` };
+    const newTask = {
+      id: idRef.current,
+      task: `task ${idRef.current}`,
+      done: false,
+    };
     setTodos([...todos, newTask]);
     localStorage.setItem("task", JSON.stringify([...todos, newTask]));
     idRef.current += 1;
@@ -31,11 +37,39 @@ function ComponentA() {
     }
   }, []);
 
+  const doneTask = (id) => {
+    const updatedTodo = todos?.map((todo) =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(updatedTodo);
+    localStorage.setItem("task", JSON.stringify(updatedTodo));
+  };
+
   return (
     <>
       <button onClick={() => createTask()}>add todo</button>
       {todos?.map((todo) => (
-        <h3 key={todo.id}>{todo.task} </h3>
+        <h3 key={todo.id}>
+          {todo.done ? (
+            <span
+              style={{
+                opacity: "50%",
+                textDecoration: "line-through",
+              }}
+            >
+              {todo.task}{" "}
+            </span>
+          ) : (
+            todo.task
+          )}{" "}
+          <button
+            onClick={() => {
+              doneTask(todo.id);
+            }}
+          >
+            done
+          </button>{" "}
+        </h3>
       ))}
     </>
   );
