7699c7767688ee51ae3ee2f90a4246279669dae9
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-26 17:36:20 +0530

    Todo: create task

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -1,27 +1,42 @@
-import { useContext } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { ThemeContext } from "./contexts/ThemeContext";
-
-import { UserContext } from "./contexts/UserContext";
 function ComponentA() {
-  const { theme, setTheme } = useContext(ThemeContext);
-  const { user, setUser } = useContext(UserContext);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      task: "react",
+    },
+    {
+      id: 2,
+      task: "node",
+    },
+  ]);
 
-  console.log(user);
+  // let id = 3;
+  const idRef = useRef(3);
+  const createTask = () => {
+    // const date = new Date();
+    // console.log(date);
+    const newTask = { id: idRef.current, task: `task ${idRef.current}` };
+    setTodos([...todos, newTask]);
+    localStorage.setItem("task", JSON.stringify([...todos, newTask]));
+    idRef.current += 1;
+    console.log(idRef.current);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("task")) {
+      const tasks = JSON.parse(localStorage.getItem("task"));
+      setTodos(tasks);
+    }
+  }, []);
+
   return (
     <>
-      <h1>Theme: {theme}</h1>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Change Theme
-      </button>
-      <h1>User: {user ? user.name : "Guest"} </h1>
-      <button
-        onClick={() =>
-          setUser({ ...user, name: "Razak Attar", city: "Bangalore" })
-        }
-      >
-        update user name
-      </button>
+      <button onClick={() => createTask()}>add todo</button>
+      {todos?.map((todo) => (
+        <h3 key={todo.id}>{todo.task} </h3>
+      ))}
     </>
   );
 }
