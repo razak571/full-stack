82fa8b662c84de429e3194ef8f642131fb179012
Razak Attar <165865038+razak571@users.noreply.github.com>
2025-09-26 20:40:08 +0530

    Todo: edit/update todo

--- frontend/src/components/hoc/test/ComponentA.jsx
+++ frontend/src/components/hoc/test/ComponentA.jsx
@@ -6,11 +6,13 @@ function ComponentA() {
       id: 1,
       task: "react",
       done: false,
+      edit: false,
     },
     {
       id: 2,
       task: "node",
       done: false,
+      edit: false,
     },
   ]);
 
@@ -23,6 +25,7 @@ function ComponentA() {
       id: idRef.current,
       task: `task ${idRef.current}`,
       done: false,
+      edit: false,
     };
     setTodos([...todos, newTask]);
     localStorage.setItem("task", JSON.stringify([...todos, newTask]));
@@ -34,6 +37,11 @@ function ComponentA() {
     if (localStorage.getItem("task")) {
       const tasks = JSON.parse(localStorage.getItem("task"));
       setTodos(tasks);
+
+      const localTodos = JSON.parse(localStorage.getItem("task"));
+      const filteredTodo = localTodos?.filter((todo) => todo.edit === true);
+      const editTodo = filteredTodo[0];
+      setEdit(editTodo?.task);
     }
   }, []);
 
@@ -45,32 +53,71 @@ function ComponentA() {
     localStorage.setItem("task", JSON.stringify(updatedTodo));
   };
 
+  const editTask = (id, task) => {
+    console.log(id);
+    const updatedTodo = todos?.map((todo) =>
+      todo.id === id ? { ...todo, edit: true } : todo
+    );
+
+    setTodos(updatedTodo);
+    localStorage.setItem("task", JSON.stringify(updatedTodo));
+    setEdit(task);
+  };
+
+  const handleUpdate = (id) => {
+    const updatedTodos = todos?.map((todo) =>
+      todo.id === id
+        ? { ...todo, task: inputRef.current.value, edit: false }
+        : todo
+    );
+
+    setTodos(updatedTodos);
+    localStorage.setItem("task", JSON.stringify(updatedTodos));
+  };
+
+  const inputRef = useRef();
+  const [edit, setEdit] = useState("default");
+
   return (
     <>
       <button onClick={() => createTask()}>add todo</button>
-      {todos?.map((todo) => (
-        <h3 key={todo.id}>
-          {todo.done ? (
-            <span
-              style={{
-                opacity: "50%",
-                textDecoration: "line-through",
+      {todos?.map((todo) =>
+        todo?.edit ? (
+          <span key={todo.id}>
+            <input
+              type="text"
+              style={{ border: "1px solid" }}
+              value={edit}
+              ref={inputRef}
+              onChange={(event) => setEdit(event.target.value)}
+            />
+            <button onClick={() => handleUpdate(todo.id)}>update</button>
+          </span>
+        ) : (
+          <h3 key={todo.id}>
+            {todo.done ? (
+              <span
+                style={{
+                  opacity: "50%",
+                  textDecoration: "line-through",
+                }}
+              >
+                {todo.task}{" "}
+              </span>
+            ) : (
+              todo.task
+            )}{" "}
+            <button
+              onClick={() => {
+                doneTask(todo.id);
               }}
             >
-              {todo.task}{" "}
-            </span>
-          ) : (
-            todo.task
-          )}{" "}
-          <button
-            onClick={() => {
-              doneTask(todo.id);
-            }}
-          >
-            done
-          </button>{" "}
-        </h3>
-      ))}
+              done
+            </button>{" "}
+            <button onClick={() => editTask(todo.id, todo.task)}>edit</button>
+          </h3>
+        )
+      )}
     </>
   );
 }
